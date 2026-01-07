38b81fe6ee6a61b7e3c4c6a0be19c9b4c9146719
shishirkhatiwada <khatiwadaccr@gmail.com>
2026-01-07 17:11:51 +0545

    Database Setup

---
+++ src/features/auth/components/auth-loading-view.tsx
@@ -0,0 +1,10 @@
+import { Spinner } from "@/components/ui/spinner"
+
+
+export const AuthLoadingView = ()=>{
+    return(
+        <div className="flex items-center justify-center h-screen bg-background">
+            <Spinner className="size-6 text-ring"/>
+        </div>
+    ) 
+}
