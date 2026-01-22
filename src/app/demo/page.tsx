1e80d3fcc210d2aff138f9692a3ea9f89bd4e0ae
shishirkhatiwada <khatiwadaccr@gmail.com>
2026-01-22 09:10:04 +0545

    06: Error handling

--- src/app/demo/page.tsx
+++ src/app/demo/page.tsx
@@ -1,38 +1,66 @@
-"use client"
-
+"use client";
+import * as Sentry from "@sentry/nextjs";
 import { Button } from "@/components/ui/button";
 import { useState } from "react";
+import { useAuth } from "@clerk/nextjs";
 
 export default function DemoPage() {
-const [loading, setLoading] = useState(false);
-const [loading2, setLoading2] = useState(false);
+  const {userId} = useAuth();
+  const [loading, setLoading] = useState(false);
+  const [loading2, setLoading2] = useState(false);
 
-    const handleBlocking = async()=>{
-        setLoading(true);
-        await fetch('/api/demo/blocking', {
-            method:'POST',
-        });
-        setLoading(false);
-    }
+  const handleBlocking = async () => {
+    setLoading(true);
+    await fetch("/api/demo/blocking", {
+      method: "POST",
+    });
+    setLoading(false);
+  };
 
+  const handleBackground = async () => {
+    setLoading2(true);
+    await fetch("/api/demo/background", {
+      method: "POST",
+    });
+    setLoading2(false);
+  };
 
-      const handleBackground = async()=>{
-        setLoading2(true);
-        await fetch('/api/demo/background', {
-            method:'POST',
-        });
-        setLoading2(false);
-    }
+  const handleClientError = () => {
+    Sentry.logger.info("user attempting to trigger a client side error", { userId });
+    throw new Error("Client Side Error: Something went wrong in the browser");
+  };
+
+  const handleApiError = async () => {
+    await fetch("/api/demo/error", {
+      method: "POST",
+    });}
+
+    const handleInngestError = async () => {
+      await fetch("/api/demo/inngest-error", { method: "POST" });
+    };
 
     return (
-          <div className="p-8 space-x-4">
-      <Button disabled={loading} onClick={handleBlocking}>
-        {loading ? 'Loading...' : 'Blocking '}
-      </Button>
+      <div className="p-8 space-x-4">
+        <Button disabled={loading} onClick={handleBlocking}>
+          {loading ? "Loading..." : "Blocking "}
+        </Button>
 
         <Button disabled={loading2} onClick={handleBackground}>
-        {loading2 ? 'Loading...' : 'UnBlocking '}
-      </Button>
-    </div>
-    )
-}
+          {loading2 ? "Loading..." : "UnBlocking "}
+        </Button>
+
+        <Button variant={"destructive"} onClick={handleClientError}>
+          Client error
+        </Button>
+
+        <Button variant={"destructive"} onClick={handleApiError}>
+          API error
+        </Button>
+
+        <Button variant={"destructive"} onClick={handleInngestError}>
+          Inngest error
+        </Button>
+      </div>
+    );
+  };
+
--- src/inngest/client.ts
+++ src/inngest/client.ts
@@ -1,5 +1,8 @@
 import {Inngest} from "inngest";
+import { sentryMiddleware } from "@inngest/middleware-sentry";
 
 export const inngest = new Inngest({
   id: "coding-agent",
+    middleware: [sentryMiddleware()],
+
 });
