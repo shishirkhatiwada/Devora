5b6bd47c9a12e1860b27b5c2a01f2e104f32f2d6
shishirkhatiwada <khatiwadaccr@gmail.com>
2026-03-24 12:01:30 +0545

    08 ide layout

--- src/features/projects/hooks/use-project.ts
+++ src/features/projects/hooks/use-project.ts
@@ -30,4 +30,46 @@ export const useCreateProject = () => {
                 localStorage.setQuery(api.projects.get, {}, [newProject, ...existingProjects]);
             }
         }
-    )}
+    )
+}
+
+export const useProject = (projectId: Id<"projects">) => {
+    return useQuery(api.projects.getById, { id: projectId });
+}
+
+export const useRenameProject = (projectId: Id<"projects">) => {
+  return useMutation(api.projects.rename).withOptimisticUpdate(
+    (localStore, args) => {
+      const existingProject = localStore.getQuery(
+        api.projects.getById,
+        { id: projectId }
+      );
+
+      if (existingProject !== undefined  && existingProject !== null) {
+        localStore.setQuery(
+          api.projects.getById,
+          { id: projectId },
+          {
+            ...existingProject,
+            name: args.name,
+            updatedAt: Date.now(),
+          }
+        );
+      }
+
+      const existingProjects = localStore.getQuery(api.projects.get);
+
+      if (existingProjects !== undefined) {
+        localStore.setQuery(
+          api.projects.get,
+          {},
+          existingProjects.map((project) => {
+            return project._id === args.id
+              ? { ...project, name: args.name, updatedAt: Date.now() }
+              : project
+          })
+        );
+      }
+    }
+  )
+};
