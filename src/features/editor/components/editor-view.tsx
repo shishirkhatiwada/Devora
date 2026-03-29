bb2b5792fa8037327a2cfef95ea6552193e5c3c7
shishirkhatiwada <khatiwadaccr@gmail.com>
2026-03-29 19:27:54 +0545

    logo fix

--- src/features/editor/components/editor-view.tsx
+++ src/features/editor/components/editor-view.tsx
@@ -40,7 +40,7 @@ export const EditorView = ({ projectId }: { projectId: Id<"projects"> }) => {
         {!activeFile && (
           <div className="size-full flex items-center justify-center">
             <Image
-              src="/logo-alt.svg"
+              src="/logo.svg"
               alt="Devora"
               width={50}
               height={50}
